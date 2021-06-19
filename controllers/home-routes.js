21c11fbc4123fd0d107fce74de0ca616b723be21
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-18 20:34:56 -0700

    create basic outline of main controller routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,30 @@
+// import routes and models
+const router = require('express').Router();
+const { Comment, Pet, Post, User } = require('../../models');
+
+// dashboard route
+router.get('/', async(req, res) => {
+
+});
+
+// new pet route
+router.get('/create-pet', (req, res) => {
+
+});
+
+// edit pet route
+router.get('/edit-pet/:id', async(req, res) => {
+
+});
+
+// new post route
+router.get('/create-post', (req, res) => {
+
+});
+
+// edit post route
+router.get('/edit-post/:id', async(req, res) => {
+
+});
+
+module.exports = router;
