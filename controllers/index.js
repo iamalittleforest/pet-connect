149ce1b5f24f711e3be11ce15f07088e805739de
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-18 19:25:03 -0700

    add forum and home route files, update index to match

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 // import routes
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const forumRoutes = require('./forum-routes');
 const homeRoutes = require('./home-routes');
 
 // prefix all routes
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/forum', forumRoutes);
 
 module.exports = router;
