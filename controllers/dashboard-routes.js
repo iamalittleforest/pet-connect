4497879ae3bffbe69156234bbf74ed6106b40600
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-22 21:15:18 -0700

    complete dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -18,13 +18,13 @@ router.get('/', withAuth, async(req, res) => {
     // serialize the data
     const users = userData.map((user) => user.get({ plain: true }));
 
-    // for rendering pg
-    res.render('dashboard-posts', { 
+    // render dashboard-info template
+    res.render('dashboard-info', { 
       layout: 'dashboard', 
       users, 
       logged_in: req.session.logged_in 
     });
-    res.status(200).json(userData);
+
   } catch (err) {
     res.status(500).json(err);
   }
@@ -32,6 +32,8 @@ router.get('/', withAuth, async(req, res) => {
 
 // new pet route
 router.get('/create-pet', withAuth, (req, res) => {
+  
+  // render create-pet template
   res.render('create-pet', { 
     layout: 'dashboard', 
     logged_in: req.session.logged_in 
@@ -39,18 +41,55 @@ router.get('/create-pet', withAuth, (req, res) => {
 });
 
 // edit pet route
-router.get('/edit-pet/:id', async(req, res) => {
+router.get('/edit-pet/:id', withAuth, async(req, res) => {
+  try {
+    const petData = await Pet.findByPk(req.params.id, {
+      include: [{ model: Comment }, { model: Post }, { model: User }]
+    });
 
+    // serialize the data
+    const pet = petData.get({ plain: true });
+
+    // render edit-pet template
+    res.render('edit-pet', { 
+      layout: 'dashboard', 
+      ...pet, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // new post route
-router.get('/create-post', (req, res) => {
-
+router.get('/create-post', withAuth, (req, res) => {
+  
+  // render create-post template
+  res.render('create-post', { 
+    layout: 'dashboard', 
+    logged_in: req.session.logged_in 
+  });
 });
 
 // edit post route
-router.get('/edit-post/:id', async(req, res) => {
+router.get('/edit-post/:id', withAuth, async(req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comment }, { model: Pet }, { model: User }]
+    });
 
+    // serialize the data
+    const post = postData.get({ plain: true });
+
+    // render edit-post template
+    res.render('edit-post', { 
+      layout: 'dashboard', 
+      ...post, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
