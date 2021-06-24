b1aaf4c41b99324387634d3606272a79e71c4fb6
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-23 21:34:35 -0700

    refactor model routes for formatting consistency

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,47 +1,33 @@
 // import routes and models
 const router = require('express').Router();
-const { Comment, Pet, Post, User } = require('../../models');
+const { Comment } = require('../../models');
+
+// import helper to prevent access unless user is logged in
+const withAuth = require('../../utils/auth');
 
 // GET all comments
 router.get('/', async (req, res) => {
-   try {
-       const commentData = await Comment.findAll({
-         include: [
-           {
-             model: User,
-           },
-         ],
-         
-       });
-     const commentsArr = commentData.map((comment) => comment.get({ plain: true }))
-     res.status(200).json(commentData);
-       //view page must be filled.
-      //  res.render('', {
-      //      commentsArr,
-      //      //Middleware loggin validation
-      //      loggedIn: req.session.loggedIn
-      //  })
-   } catch (err) {
-       console.log(err);
-       res.status(500).json(err);
-   }
+  try {
+    const commentData = await Comment.findAll();
 
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // CREATE new comment
-router.post('/', async(req, res) => {
-    try {
-        const commentData = await Comment.create({
-        ...req.body
-        })
-        res.status(200).json(commentData)
-    } catch (error) {
-
-      console.log(err)
-        res.status(400).json({ message: 'No comment'})
-
-    }
+router.post('/', withAuth, async(req, res) => {
+  try {
+    const commentData = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id
+    });
     
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
