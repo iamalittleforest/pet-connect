835b3614ab73e68005ead4ff3e3308858d9ff62d
Rod Blanco <rodrigo.a.blanco1990@gmail.com>
2021-06-25 12:58:03 -0700

    cleaned up add pet view and create post view/js

--- public/js/create-pet.js
+++ public/js/create-pet.js
@@ -1,4 +1,27 @@
-const petName = document.querySelector('#petName');
-const feline = document.querySelector("#felineOption");
-const canine = document.querySelector("#canineOption");
-const petAge = document.querySelector("#petAge");
+//rough draft
+const createPethandler = async (event) => {
+    event.preventDefault();
+//collect inputs
+const petName = document.querySelector('#petName').value.trim();
+const feline = document.querySelector("#felineOption").value.trim();
+const canine = document.querySelector("#canineOption").value.trim();
+const petAge = document.querySelector("#petAge").value.trim();
+
+
+
+    if (petName && feline && canine && petAge) {
+        const response = await fetch('/api/pets', {
+            method: "POST",
+            body: JSON.stringify({petName, feline, canine, petAge}),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+document
+    .querySelector(".pet-info").addEventListener('submit', createPethandler);
