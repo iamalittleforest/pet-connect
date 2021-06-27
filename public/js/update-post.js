000a6cb33ba23de0822427f449e047ebd59099c8
Omri <omri.ronen4@gmail.com>
2021-06-27 14:56:35 -0700

    added 2 files one for update & one for delete. Also added the scripts for these files on the handlebars of edit post

---
+++ public/js/update-post.js
@@ -0,0 +1,40 @@
+// collect inputs from radio buttons
+let category = document.querySelector('#category').value.trim();
+Array.from(document.getElementsByClassName("radio")).forEach((a) => {
+  a.addEventListener("click", function () {
+    category = this.value;
+    console.log(category);
+  });
+});
+
+// define function to update post
+const updatePost = async (event) => {
+  event.preventDefault();
+
+  // collect inputs
+  const id = window.location.toString().split('/').slice(-1)[0];
+  const title = document.querySelector('#edit-post-title').value.trim();
+  const description = document.querySelector('#edit-post-description').value.trim();
+  // console.log(id, title, category, description);
+
+  if (id && title && category && description) {
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, category, description }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    // redirect to dashboard if post update is successful
+    if (response.ok) {
+      document.location.replace(`/dashboard`);
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// listen for click
+document
+  .querySelector('#update-post-btn')
+  .addEventListener('click', updatePost());
+
