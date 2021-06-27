4bed5bcd313c8fc0df62a9a759ec343758d65d35
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-06-27 11:46:36 -0700

    add conditionals to views, create helper file, update server, update js files

--- public/js/edit-pet.js
+++ public/js/edit-pet.js
@@ -1,7 +1,5 @@
-// identify which radio button is checked and display
-
 // collect inputs from radio buttons
-let gender = null;
+let gender = document.querySelector('#gender').value.trim();
 Array.from(document.getElementsByClassName("radio")).forEach((a) => {
   a.addEventListener("click", function () {
     gender = this.value;
@@ -14,13 +12,14 @@ const updatePet = async (event) => {
   event.preventDefault();
 
   // collect inputs from textarea
+  const id = window.location.toString().split('/').slice(-1)[0];
   const name = document.querySelector('#edit-pet-name').value.trim();
   const species = document.querySelector('#edit-pet-species').value.trim();
   const breed = document.querySelector('#edit-pet-breed').value.trim();
   const age = document.querySelector('#edit-pet-age').value.trim();
-  console.log(name && species && breed && gender && age);
+  // console.log(name && species && breed && gender && age);
 
-  if (name && species && breed && gender && age) {
+  if (id && name && species && breed && gender && age) {
     const response = await fetch(`/api/pets/${id}`, {
       method: 'PUT',
       body: JSON.stringify({ name, species, breed, gender, age }),
@@ -36,7 +35,7 @@ const updatePet = async (event) => {
   }
 };
 
-// listen for submit
+// listen for click
 document
   .querySelector('#update-pet-btn')
   .addEventListener('click', updatePet());
@@ -63,7 +62,7 @@ const deletePet = async (event) => {
   }
 };
 
-// listen for submit
+// listen for click
 document
   .querySelector('#delete-pet-btn')
   .addEventListener('click', deletePet());
---
+++ utils/helpers.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+  // compares saved gender with gender options
+  checkGender: (a, b) => {
+    console.log(a, b);
+    const isEqual = a == b;
+    return isEqual;
+  },
+
+  // compares saved category with category options
+  checkCategory: (a, b) => {
+    console.log(a, b);
+    const isEqual = a == b;
+    return isEqual;
+  }
+}
